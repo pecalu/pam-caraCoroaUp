da06572f939be4b82277e290bffc5827f0947c19
pecalu <124682526+pecalu@users.noreply.github.com>
2024-05-21 12:17:34 -0300

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avaliacaoPAM.Models
+{
+    public class Coin
+    {
+        public string Lado { get; set; }
+
+        public string jogar()
+        {
+            int ladoSorteado = new Random().Next(2);
+            //o é cara
+            //1 é coroa
+            Lado = ladoSorteado == 0 ? "cara" : "coroa";
+            return(Lado); 
+        }
+
+        public string jogar(string ladoEscolhido)
+        {
+            int ladoSorteado = new Random().Next(2);
+            Lado = ladoSorteado == 0 ? "cara" : "coroa";
+            string resultado = Lado == ladoEscolhido ? "Você acertou!" : "Você errou!";
+            return (resultado);
+        }
+    }
+}
